80afe4040bbf3eb1b238f0b05c63a8688fc05989
Jeong_geumdam <suseongland@gmail.com>
2021-04-02 23:37:36 +0900

    Updates

---
+++ precache-manifest.bd0dfb56306fa00c43e23336922ac18a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018d840d988118946c24505b1fd01584",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f7c02e77b92c706a5251",
+    "url": "/portfolio/static/css/main.e13ff1f6.chunk.css"
+  },
+  {
+    "revision": "2cf08447aecb80e939dc",
+    "url": "/portfolio/static/js/2.365c1471.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/portfolio/static/js/2.365c1471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c02e77b92c706a5251",
+    "url": "/portfolio/static/js/main.f790ac4e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "bfe1e66272ffebea04e1aee77d10a769",
+    "url": "/portfolio/static/media/algospot_1.bfe1e662.PNG"
+  },
+  {
+    "revision": "5a7d77a8e8fb6779c793fb6bd4c46eb1",
+    "url": "/portfolio/static/media/algospot_2.5a7d77a8.PNG"
+  },
+  {
+    "revision": "577200bed58d5a3a887d58a7147c630d",
+    "url": "/portfolio/static/media/algospot_3.577200be.PNG"
+  },
+  {
+    "revision": "38f7d61ba00962df140fd94ec8db88b1",
+    "url": "/portfolio/static/media/algospot_4.38f7d61b.PNG"
+  },
+  {
+    "revision": "69394f7e8f37036f090e1956a741405d",
+    "url": "/portfolio/static/media/codeit_1.69394f7e.PNG"
+  },
+  {
+    "revision": "7a27a65f8d92b051fe4daa53f10b9891",
+    "url": "/portfolio/static/media/codeit_2.7a27a65f.PNG"
+  },
+  {
+    "revision": "46bda6cbd33a5715303e4a393d406c82",
+    "url": "/portfolio/static/media/nomadcoder_1.46bda6cb.PNG"
+  },
+  {
+    "revision": "048cb6a85a864365b183b3018d7a7adb",
+    "url": "/portfolio/static/media/nomadcoder_2.048cb6a8.PNG"
+  },
+  {
+    "revision": "72545f9e3612bb2ae5a4a517741714de",
+    "url": "/portfolio/static/media/nomadcoder_3.72545f9e.PNG"
+  },
+  {
+    "revision": "0cbd08fe665fc3b4269f7d5e532e636a",
+    "url": "/portfolio/static/media/nomadcoder_4.0cbd08fe.PNG"
+  }
+]);
